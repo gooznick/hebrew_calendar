3500e0069fc305365f013f1e39e4ac4f3641b328
gooznick <gooznick@gmail.com>
2022-04-26 21:45:26 +0300

    black

--- src/gematria.py
+++ src/gematria.py
@@ -1,14 +1,13 @@
-
-
 # prepare the values dictionary
 chars = "אבגדהוזחטיכלמנסעפצקרשת"
-val = {chars[v]:v+1 for v in range(10)}
-val.update({chars[v]:(v-8)*10 for v in range(10,19)})
-val.update({chars[v]:(v-17)*100 for v in range(19,22)})
+val = {chars[v]: v + 1 for v in range(10)}
+val.update({chars[v]: (v - 8) * 10 for v in range(10, 19)})
+val.update({chars[v]: (v - 17) * 100 for v in range(19, 22)})
 ends = ("מנצפכ", "םןץףך")
-val.update({e:val[v] for v,e in zip(ends[0], ends[1])})
+val.update({e: val[v] for v, e in zip(ends[0], ends[1])})
 
-def gematria(word : str):
+
+def gematria(word: str):
     """
     Convert a word to it's numeric value
     Support using first letter for thousands
@@ -28,6 +27,6 @@ def gematria(word : str):
     """
     thousands = 0
     if word[1] in "-'`":
-        thousands = val.get(word[0],0)*1000
-        word=word[2:]
-    return thousands + sum([val.get(c,0) for c in word])
+        thousands = val.get(word[0], 0) * 1000
+        word = word[2:]
+    return thousands + sum([val.get(c, 0) for c in word])
