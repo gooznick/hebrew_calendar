30f7f1b90942c51289542b78178bb51a0a380ea7
gooznick <gooznick@gmail.com>
2022-12-20 19:30:16 +0200

    using x=x0+(t-t0)*v

--- src/sun.py
+++ src/sun.py
@@ -24,20 +24,40 @@ class Sun:
     @staticmethod
     def mean_location(date: HDate):
         """
-        Compute the mean location of the sun in the beginning of a day
+        Compute the mean location of the sun in a specific day
+        It may be the location in the beginning of the night, 
+        or an hour before sunset, or an hour after sunset # פרק יב הלכה ב
         """
-        days = molad.Months.days_diff(BeginningDay, date)
-        location = LocationInBeginningDay + OneDayMeanMovement*days
+        return Sun.__compute_location_on_day(date, location_coefs)
+
+    @staticmethod
+    def aphelion(date: HDate):
+        """
+        Compute the aphelion of the sun in a specific day
+        It may be the location in the beginning of the night, 
+        or an hour before sunset, or an hour after sunset # פרק יב הלכה ב
+        """
+        return Sun.__compute_location_on_day(date, aphelion_coefs)
+
+    @staticmethod
+    def __compute_location_on_day(date: HDate, coefficients):
+        days_since_t0 = molad.Months.days_diff(coefficients["t0"], date)
+        location = coefficients["x0"] + coefficients["v"]*days_since_t0
+        return location.remove_circles()
+
+    @staticmethod
+    def aphelion(date: HDate):
+        """
+        Compute the aphelion of the sun in a specific day
+        """
+        days = molad.Months.days_diff(location["t0"], date)
+        location = location["x0"] + location["v"]*days
 
         return location.remove_circles()
 
 
-OneDayMeanMovement = angle(27*360 + gematria.str_to_num("קלו"),
-                           "כח", "כ") / 10000  # פרק יב הלכה א
-
-OneDayAphelionMovement = angle(0, 0, 1, "ל") / 10  # פרק יב הלכה ב
-
-BeginningDay = HDate("ג", "ניסן", "ד-תתקלח")
-AphelionInBeginningDay = angle(
-    "כו", "מה", 8) + angle(gematria.mazal_to_degree("תאומים"))  # פרק יב הלכה ב
-LocationInBeginningDay = angle(7, 3, "לב")   # פרק יב הלכה ב
+RambamBeginningDay = HDate("ג", "ניסן", "ד-תתקלח")
+location_coefs = {"t0": RambamBeginningDay, "x0": angle(7, 3, "לב"), "v": angle(27*360 + gematria.str_to_num("קלו"),
+                                                                                "כח", "כ") / 10000}  # פרק יב הלכה א
+aphelion_coefs = {"t0": RambamBeginningDay, "x0": angle(
+    "כו", "מה", 8) + angle(gematria.mazal_to_degree("תאומים")), "v": angle(0, 0, 1, "ל") / 10}  # פרק יב הלכה א
--- test/test_ch_12.py
+++ test/test_ch_12.py
@@ -7,37 +7,38 @@ import molad
 
 
 def test_halacha_1():
-    assert ((sun.OneDayMeanMovement * 10).remove_thirds().remove_circles()
+    assert ((sun.location_coefs["v"] * 10).remove_thirds().remove_circles()
             == angle(9, 51, 23))
-    assert ((sun.OneDayMeanMovement * 100).remove_thirds().remove_circles()
+    assert ((sun.location_coefs["v"] * 100).remove_thirds().remove_circles()
             == angle(98, 33, 53))
-    assert ((sun.OneDayMeanMovement * 1000).remove_thirds().remove_circles()
+    assert ((sun.location_coefs["v"] * 1000).remove_thirds().remove_circles()
             == angle("רסה", "לח", "נ"))
-    assert ((sun.OneDayMeanMovement * 10000).remove_thirds().remove_circles()
+    assert ((sun.location_coefs["v"] * 10000).remove_thirds().remove_circles()
             == angle("קלו", "כח", "כ"))
-    assert ((sun.OneDayMeanMovement * 29).remove_thirds().remove_circles()
+    assert ((sun.location_coefs["v"] * 29).remove_thirds().remove_circles()
             == angle("כח", "לה", 1))
 
 
 def test_halacha_2():
-    assert ((sun.OneDayAphelionMovement * 10).remove_circles()
+    assert ((sun.aphelion_coefs["v"] * 10).remove_circles()
             == angle(0, 0, 1, "ל"))
-    assert ((sun.OneDayAphelionMovement * 100).remove_circles()
+    assert ((sun.aphelion_coefs["v"] * 100).remove_circles()
             == angle(0, 0, "טו"))
-    assert ((sun.OneDayAphelionMovement * 1000).remove_circles()
+    assert ((sun.aphelion_coefs["v"] * 1000).remove_circles()
             == angle(0, 2, 30))
-    assert ((sun.OneDayAphelionMovement * 10000).remove_circles()
+    assert ((sun.aphelion_coefs["v"] * 10000).remove_circles()
             == angle(0, 25))
-    assert ((sun.OneDayAphelionMovement * 29).remove_thirds().remove_circles()
+    assert ((sun.aphelion_coefs["v"] * 29).remove_thirds().remove_circles()
             == angle(0, 0, 4))
-    assert ((sun.OneDayAphelionMovement * 354).remove_thirds().remove_circles()
+    assert ((sun.aphelion_coefs["v"] * 354).remove_thirds().remove_circles()
             == angle(0, 0, "נג"))
 
-    assert (molad.Months.weekday(sun.BeginningDay) == 5)
-    beginning_year = sun.BeginningDay._year
+    assert (molad.Months.weekday(sun.location_coefs["t0"]) == 5)
+    beginning_year = sun.location_coefs["t0"]._year
     wanted_date = HDate("יד", "תמוז", beginning_year)
 
-    assert (100 == molad.Months.days_diff(sun.BeginningDay, wanted_date))
+    assert (100 == molad.Months.days_diff(
+        sun.location_coefs["t0"], wanted_date))
     location = sun.Sun.mean_location(wanted_date)
     assert (location == angle(105, "לז", "כה"))
 
@@ -46,5 +47,5 @@ def test_halacha_2():
 
 @pytest.mark.xfail
 def test_halacha_1_year():
-    assert ((sun.OneDayMeanMovement * 354).remove_thirds().remove_circles()
+    assert ((sun.location_coefs["v"] * 354).remove_thirds().remove_circles()
             == angle("שמח", "נה", "טו"))
